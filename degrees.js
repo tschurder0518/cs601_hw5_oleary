dc633c3bb6a5398b09a68114a264f5659ee8085b
tschurder0518 <tschurder0518@gmail.com>
2021-06-13 18:31:14 -0500

    Uploaded Files

---
+++ degrees.js
@@ -0,0 +1,27 @@
+(function() {
+    var httpRequest;
+    document.getElementById("button").onclick = function() { makeRequest('my_degrees.json')
+};
+
+function makeRequest(url) {
+    httpRequest = new XMLHttpRequest();
+    if (!httpRequest) {
+        alert('Error: Unable to create an XMLHTTP instance');
+        return false;
+    }
+    httpRequest.onreadystatechange = tableContents;
+
+    httpRequest.open('GET', url);
+    httpRequest.send();
+}
+
+function tableContents() {
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        if (httpRequest.status === 200) {
+            document.write(httpRequest.responseText);
+        } else {
+            alert("There was a problem with the request");
+        }
+    }
+}
+})();
