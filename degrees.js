991fbcc187b693859a1424dcabcf344df17d8c07
tschurder0518 <tschurder0518@gmail.com>
2021-06-14 17:25:56 -0500

    Update degrees.js

--- degrees.js
+++ degrees.js
@@ -17,7 +17,7 @@ function loadJson() {
 
 function loadTable(httpInfo) {
     var jsonInfo = httpInfo.responseText;
-    document.write(jsonInfo.degrees[0].degree.school;
+    document.write(jsonInfo.degrees[0].degree.school);
 //     var parsed = JSON.parse(jsonInfo);
 //     var degreeTable = "<tr><th>School</th><th>Major</th><th>Degree Type</th><th>Year Complete</th></tr>";
 //     var pageContent = '';
